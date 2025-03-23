98c3df8d14961d3f0e2f4bf4c71d4adcf28291ef
johnny nylund <john_newcreek@protonmail.com>
2025-03-23 22:53:02 +0200

    update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>👋 Hi, I'm [Your Name]</h1>
-      <p>I'm a [Your Role] specializing in [Tech Stack].</p>
+      <h1>👋 Hi, I'm Johnny Nylund</h1>
+      <p>I'm a robot process automation engineer currently working at Digia Finland Oy.</p>
       <a href="/projects">View My Work</a> | <a href="/contact">Get in Touch</a>
     </main>
   );
