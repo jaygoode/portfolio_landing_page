98c0e9ba06ac84a571b82a40c5839f02fc741ac8
johnny nylund <john_newcreek@protonmail.com>
2025-03-31 22:37:19 +0300

    todo updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,13 @@ export default function Home() {
   return (
     <main>
       <section>
+
       <h1>👋 Hi, I'm Johnny Nylund</h1>
+
       <p>I'm a robot process automation engineer currently working at Digia Finland Oy.</p>
+
       <a href="/projects">View My Work</a> | <a href="/contact">Get in Touch</a>
+      
       </section>
 
       <section id="about" className="h-screen flex items-center justify-center bg-gray-200">
