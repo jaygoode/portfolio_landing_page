ee542f14de66186b28c70ce781a171d2c8cf1961
johnny nylund <john_newcreek@protonmail.com>
2025-07-01 17:10:36 +0300

    removed old navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,7 @@ export default function Home() {
 
       <h1>👋 Hi, I'm Johnny Nylund</h1>
 
-      <p>I'm a robot process automation engineer currently working at Digia Finland Oy.</p>
-
-      <a href="/projects">View My Work</a> | <a href="/contact">Get in Touch</a>
+      <p>I'm a robot process automation engineer.</p>
 
       </section>
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -18,16 +18,3 @@ export default function Navbar() {
   </nav>
   )
 }
-//   return (
-//     <nav className="fixed top-0 left-0 w-full bg-white shadow-md p-4 z-10">
-//       <ul className="flex space-x-6 justify-center">
-//       <li><a href="#home" className="hover:text-blue-500">Home</a></li>
-
-//         <li><a href="#about" className="hover:text-blue-500">About</a></li>
-//         <li><a href="#projects" className="hover:text-blue-500">Projects</a></li>
-//         <li><a href="#contact" className="hover:text-blue-500">Contact</a></li>
-//         <li><a href="#testimonials" className="hover:text-blue-500">Testimonials</a></li>
-//       </ul>
-//     </nav>
-//   );
-// }
